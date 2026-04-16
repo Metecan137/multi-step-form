767b150810dd3d2e21b616c076ea24d3304fc6ec
Metecan Cikrikcioglu <metecancikrikcioglu57@gmail.com>
2026-04-16 14:55:07 +0300

    sidebarlar tamam

--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -1,13 +1,16 @@
+import { useSelector } from "react-redux"
 import sidebarMobile from "../assets/images/bg-sidebar-mobile.svg"
+import { buttonCount } from "../features/multiformSlice/multiformSlice"
 
 function MobileSidebar() {
+  const buttonNumber = useSelector(buttonCount)
   return (
     <div className="relative">
       <img src={sidebarMobile} alt="sidebarMobile" className="w-full" />
       <div className="absolute right-1/2 translate-x-1/2 top-1/5 flex gap-4 text-white text-sm font-bold">
         {
           [1, 2, 3, 4].map((item) => (
-            <div key={item} className="rounded-full border w-8 h-8 sm:w-10 sm:h-10 flex items-center justify-center transition-all">{item}</div>
+            <div key={item} className={`${buttonNumber === item ? "bg-primary-blue-300" : ""} rounded-full border w-8 h-8 sm:w-10 sm:h-10 flex items-center justify-center transition-all`}>{item}</div>
           ))
         }
       </div>
